234291bf2a8d06b10980af5dfb19788aa88c5ec7
Hecc06 <thevillagergamer28@gmail.com>
2021-11-28 14:21:03 -0500

    Uppercase

--- main.js
+++ main.js
@@ -18,18 +18,18 @@ const active = new Map();
 
 //Command handling
 client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./Commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const command = require(`./Commands/${file}`);
 	client.commands.set(command.data.name, command);
 }
 
 //Event Handler
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./Events').filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
+	const event = require(`./Events/${file}`);
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args));
 	} else {
